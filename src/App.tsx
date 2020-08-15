49b6c4c20094733a367dba65706d20bc62825fbc
Konstantin Kirilin <cons.kirilin@gmail.com>
2020-08-15 16:30:39 +0300

    main files

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import PokemonsList from "./pages/PokemonsList/PokemonsList";
+import PokemonProfile from "./pages/PokemonProfile/PokemonProfile";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/:id">
+            <PokemonProfile />
+          </Route>
+          <Route exact path="/">
+            <PokemonsList />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
